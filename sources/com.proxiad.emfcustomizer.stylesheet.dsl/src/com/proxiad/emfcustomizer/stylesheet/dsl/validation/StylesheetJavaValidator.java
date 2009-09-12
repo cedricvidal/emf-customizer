65e9eff5c0410db139103ecd11f2328e155bc2df
Cédric Vidal <cedric@vidal.biz>
2009-09-12 15:29:54 +0200

    Configured checkstyle eclipse plugin For now, checkstyle configuration only checks for proper header in java source files The checkstyle configuration is in dedicated build project com.proxiad.emfcustomizer.build

--- sources/com.proxiad.emfcustomizer.stylesheet.dsl/src/com/proxiad/emfcustomizer/stylesheet/dsl/validation/StylesheetJavaValidator.java
+++ sources/com.proxiad.emfcustomizer.stylesheet.dsl/src/com/proxiad/emfcustomizer/stylesheet/dsl/validation/StylesheetJavaValidator.java
@@ -1,5 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2009 Cedric Vidal and ProxiAD Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Cedric Vidal - original idea, initial contribution and API
+ *     ProxiAD Group - 1.0 release
+ *******************************************************************************/
+
 package com.proxiad.emfcustomizer.stylesheet.dsl.validation;
- 
 
 public class StylesheetJavaValidator extends AbstractStylesheetJavaValidator {
 
