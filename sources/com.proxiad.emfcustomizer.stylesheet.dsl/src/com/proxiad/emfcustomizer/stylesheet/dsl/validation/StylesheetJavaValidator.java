92d0d651086816df8458e22ba823f28605d766c7
Cédric Vidal <cedric@vidal.biz>
2009-09-12 16:17:49 +0200

    Adding validation checks

--- sources/com.proxiad.emfcustomizer.stylesheet.dsl/src/com/proxiad/emfcustomizer/stylesheet/dsl/validation/StylesheetJavaValidator.java
+++ sources/com.proxiad.emfcustomizer.stylesheet.dsl/src/com/proxiad/emfcustomizer/stylesheet/dsl/validation/StylesheetJavaValidator.java
@@ -12,13 +12,70 @@
 
 package com.proxiad.emfcustomizer.stylesheet.dsl.validation;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtext.validation.Check;
+
+import com.proxiad.emfcustomizer.ecss.AttributeDefinition;
+import com.proxiad.emfcustomizer.ecss.EcssPackage;
+import com.proxiad.emfcustomizer.ecss.Element;
+import com.proxiad.emfcustomizer.ecss.ReferenceDefinition;
+import com.proxiad.emfcustomizer.ecss.Style;
+import com.proxiad.emfcustomizer.ecss.util.Constraints;
+import com.proxiad.emfcustomizer.ecss.util.Queries;
+
 public class StylesheetJavaValidator extends AbstractStylesheetJavaValidator {
 
-//	@Check
-//	public void checkTypeNameStartsWithCapital(Type type) {
-//		if (!Character.isUpperCase(type.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
-//		}
-//	}
+	/**
+	 * 
+	 * @param attributeDefinition
+	 */
+	@Check
+	public void checkAttributeDefinitionValueCompatibility(
+			AttributeDefinition attributeDefinition) {
+		Object value = Queries.value(attributeDefinition.getValue());
+		if (value != null
+				&& !Constraints.isCompatible(attributeDefinition, value)) {
+			EStructuralFeature attribute = attributeDefinition.getAttribute();
+			error(value + " is incompatible with attribute "
+					+ attribute.getName(),
+					EcssPackage.ATTRIBUTE_DEFINITION__VALUE);
+		}
+	}
+
+	/**
+	 * 
+	 * @param referenceDefinition
+	 */
+	@Check
+	public void checkReferenceDefinitionValueCompatibility(
+			ReferenceDefinition referenceDefinition) {
+		EObject value = referenceDefinition.getModelRef();
+		if (value != null
+				&& !Constraints.isCompatible(referenceDefinition, value)) {
+			EStructuralFeature attribute = referenceDefinition.getAttribute();
+			error("This " + value.eClass().getName()
+					+ " is incompatible with attribute " + attribute.getName(),
+					EcssPackage.ATTRIBUTE_DEFINITION__VALUE);
+		}
+	}
+
+	@Check
+	// TODO: Change the grammar ?
+	public void checkNotNullDefinitionInStyle(Style style) {
+		if (style.getDefinitions().size() == 0) {
+			error("At least a Definition in Style must be defined",
+					EcssPackage.STYLE__DEFINITIONS);
+		}
+	}
+
+	@Check
+	// TODO: Change the grammar ?
+	public void checkNotNullDefinitionInElement(Element element) {
+		if (element.getDefinitions().size() == 0) {
+			error("At least a Definition in Element must be defined",
+					EcssPackage.ELEMENT__DEFINITIONS);
+		}
+	}
 
 }
